d84869554e784484dd0cb2a24c4a0dddf9142c57
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-07-01 12:45:08 +0530

    Update bot.js

--- G9/java/bot.js
+++ G9/java/bot.js
@@ -22,63 +22,63 @@
 const teachers = {
   // Sinhala
   STIN: { name: "S. T. Indrani", subjects: "Sinhala, Optional-2", img: "Image/STIN.png" },//
-  STIN2: { name: "S. T. Indrani", subjects: "Tutorials Sinhala", img: "Image/STIN.JPG" },//
+  STIN2: { name: "S. T. Indrani", subjects: "Tutorials Sinhala", img: "Image/STIN.png" },//
   SANP: { name: "S. A. Nayana Preethi Kumari", subjects: "Sinhala", img: "Image/SANP.png" },//
-  SWM: { name: "S. W. M. Krishantha", subjects: "Sinhala, Tutorials Sinhala", img: "Image/SWM.JPG" },
-  NUWA: { name: "G. H. N. Chathuranga", subjects: "Sinhala", img: "Image/NUWA.JPG" },//
-  MDMU: { name: "M.D.M.U. Pushpakumara", subjects: "Sinhala", img: "Image/MDMU.JPG" },
-  YASK: { name: "Y.A.S.K. Wijerathna", subjects: "Sinhala", img: "Image/YASK.JPG" },
-  HKD: { name: "H.K. Dinesh", subjects: "Sinhala", img: "Image/HKD.JPG" },
+  SWM: { name: "S. W. M. Krishantha", subjects: "Sinhala, Tutorials Sinhala", img: "Image/SWM.png" },
+  NUWA: { name: "G. H. N. Chathuranga", subjects: "Sinhala", img: "Image/NUWA.png" },//
+  MDMU: { name: "M.D.M.U. Pushpakumara", subjects: "Sinhala", img: "Image/MDMU.png" },
+  YASK: { name: "Y.A.S.K. Wijerathna", subjects: "Sinhala", img: "Image/YASK.png" },
+  HKD: { name: "H.K. Dinesh", subjects: "Sinhala", img: "Image/HKD.png" },
   // English
-  IUG: { name: "I. U. Godagama", subjects: "English", img: "Image/IUG.JPG" },
-  SDJP: { name: "S. D. R. J. P. Samarakoon", subjects: "English", img: "Image/SDJP.JPG" },
-  SMYD: { name: "S. M. Y. Darshana", subjects: "English", img: "Image/SMYD.JPG" },
-  HRNG: { name: "H.R.N. Gayantha", subjects: "English", img: "Image/HRNG.JPG" },
+  IUG: { name: "I. U. Godagama", subjects: "English", img: "Image/IUG.png" },
+  SDJP: { name: "S. D. R. J. P. Samarakoon", subjects: "English", img: "Image/SDJP.png" },
+  SMYD: { name: "S. M. Y. Darshana", subjects: "English", img: "Image/SMYD.png" },
+  HRNG: { name: "H.R.N. Gayantha", subjects: "English", img: "Image/HRNG.png" },
 
   // Mathematics
-  MKRC: { name: "M. K. Rasika Nishantha", subjects: "Mathematics", img: "Image/MKRC.JPG" },
-  HKAU: { name: "H. K. A. Udayangi", subjects: "Mathematics", img: "Image/HKAU.JPG" },
-  GHDC: { name: "T. H. D. Chathulakkana", subjects: "Mathematics", img: "Image/GHDC.JPG" },
+  MKRC: { name: "M. K. Rasika Nishantha", subjects: "Mathematics", img: "Image/MKRC.png" },
+  HKAU: { name: "H. K. A. Udayangi", subjects: "Mathematics", img: "Image/HKAU.png" },
+  GHDC: { name: "T. H. D. Chathulakkana", subjects: "Mathematics", img: "Image/GHDC.png" },
   BPLA: { name: "B. P. Lamahewa", subjects: "Mathematics", img: "Image/BPLA.png" },
-  BDSE: { name: "B. D. Senanayeka", subjects: "Mathematics", img: "Image/BDSE.JPG" },
-  GISA: { name: "G. Iresha Sanjeewani", subjects: "Mathematics", img: "Image/GISA.JPG" },
-  NMHS: { name: "N.M.U.S.K. Nawarathne", subjects: "Mathematics", img: "Image/NMHS.JPG" },
+  BDSE: { name: "B. D. Senanayeka", subjects: "Mathematics", img: "Image/BDSE.png" },
+  GISA: { name: "G. Iresha Sanjeewani", subjects: "Mathematics", img: "Image/GISA.png" },
+  NMHS: { name: "N.M.U.S.K. Nawarathne", subjects: "Mathematics", img: "Image/NMHS.png" },
 
 
   // Buddhism
   KSRI: { name: "K. Sirisumana", subjects: "Buddhism, Buddhism Tutorial Sciences", img: "Image/KSRI.png" },
   MACH: { name: "Makumbura Chandraloka", subjects: "Buddhism", img: "Image/MACH.png" },
-  DGSR: { name: "D. G. S. R. Dodangoda", subjects: "Buddhism, Science Practical", img: "Image/DGSR.JPG" },
+  DGSR: { name: "D. G. S. R. Dodangoda", subjects: "Buddhism, Science Practical", img: "Image/DGSR.png" },
 
   // Science
-  SCI: { name: "D. G. S. R. Dodangoda", subjects: "Science", img: "Image/SCI.JPG" },
+  SCI: { name: "D. G. S. R. Dodangoda", subjects: "Science", img: "Image/SCI.png" },
   NPAM: { name: "N. P. Amila Chaturika", subjects: "Science", img: "Image/NPAM.png" },
   WHG: { name: "W. H. G. Nishani", subjects: "Science", img: "Image/WHG.png" },
-  HLDP: { name: "H. L. D. P. Champika", subjects: "Science, Tutorial Sciences", img: "Image/HLDP.JPG" },
-  IAGA: { name: "I.A. Gamage", subjects: "Science, Tutorial Sciences", img: "Image/IAGA.JPG" },
+  HLDP: { name: "H. L. D. P. Champika", subjects: "Science, Tutorial Sciences", img: "Image/HLDP.png" },
+  IAGA: { name: "I.A. Gamage", subjects: "Science, Tutorial Sciences", img: "Image/IAGA.png" },
 
   // History
-  KHNL: { name: "K. H. Nallaperuma", subjects: "History, Tutriols History", img: "Image/KHNL.JPG" },
-  MVHD: { name: "M. V. H. D. Gunasekara", subjects: "History", img: "Image/MVHD.JPG" },
-  WARW: { name: "W. A. R. Wickramarachchi", subjects: "History, Tutriols History", img: "Image/WARW.JPG" },
-  ASMA: { name: "A.S. Manoja", subjects: "History, Tutriols History", img: "Image/ASMA.JPG" },
+  KHNL: { name: "K. H. Nallaperuma", subjects: "History, Tutriols History", img: "Image/KHNL.png" },
+  MVHD: { name: "M. V. H. D. Gunasekara", subjects: "History", img: "Image/MVHD.png" },
+  WARW: { name: "W. A. R. Wickramarachchi", subjects: "History, Tutriols History", img: "Image/WARW.png" },
+  ASMA: { name: "A.S. Manoja", subjects: "History, Tutriols History", img: "Image/ASMA.png" },
 
   // Optional Subjects
-  RDNR: { name: "R. D. N. Rathnayaka", subjects: "Optional 1", img: "Image/RDNR.JPG" },
-  MWA: { name: "M. W. Ajith", subjects: "Optional-3", img: "Image/MWA.JPG" },
-  WTKK: { name: "W. T. K. Koshali", subjects: "Optional-3", img: "Image/WTKK.JPG" },
-  AMRA: { name: "A. M. Ranasingha Adikari", subjects: "Optional-2", img: "Image/AMRA.JPG" },
-  NTWE: { name: "Nilmini Wewalwala", subjects: "Optional-2", img: "Image/NTWE.JPG" },
-  PDKW: { name: "P. D. Kariyawasam", subjects: "Optional-2", img: "Image/PDKW.JPG" },
-  MMGG: { name: "M. M. Gothami Gunapali", subjects: "Optional-2", img: "Image/MMGG.JPG" },
-  ABCP: { name: "B. A. C. Priyakalum", subjects: "Optional-2", img: "Image/ABCP.JPG" },
+  RDNR: { name: "R. D. N. Rathnayaka", subjects: "Optional 1", img: "Image/RDNR.png" },
+  MWA: { name: "M. W. Ajith", subjects: "Optional-3", img: "Image/MWA.png" },
+  WTKK: { name: "W. T. K. Koshali", subjects: "Optional-3", img: "Image/WTKK.png" },
+  AMRA: { name: "A. M. Ranasingha Adikari", subjects: "Optional-2", img: "Image/AMRA.png" },
+  NTWE: { name: "Nilmini Wewalwala", subjects: "Optional-2", img: "Image/NTWE.png" },
+  PDKW: { name: "P. D. Kariyawasam", subjects: "Optional-2", img: "Image/PDKW.png" },
+  MMGG: { name: "M. M. Gothami Gunapali", subjects: "Optional-2", img: "Image/MMGG.png" },
+  ABCP: { name: "B. A. C. Priyakalum", subjects: "Optional-2", img: "Image/ABCP.png" },
 
   // Practical and Tutorials
-  SCPR: { name: "D. G. S. R. Dodangoda", subjects: "Science Practical", img: "Image/SCPR.JPG" },
-  LAB1: { name: "Lab1", subjects: "Science Practical", img: "Image/LAB1.JPG" },
-  TUTS: { name: "H. L. D. P. Champika", subjects: "Tutorial Sciences", img: "Image/TUTS.JPG" },
-  TUTH: { name: "M. V. H. D. Gunasekara", subjects: "Tutriols History", img: "Image/TUTH.JPG" },
-  TUSI: { name: "S. T. Indrani", subjects: "Tutorials Sinhala", img: "Image/TUSI.JPG" },
-  LIOB: { name: "MRS. Library", subjects: "Library", img: "Image/LIOB.JPG" }
+  SCPR: { name: "D. G. S. R. Dodangoda", subjects: "Science Practical", img: "Image/SCPR.png" },
+  LAB1: { name: "Lab1", subjects: "Science Practical", img: "Image/LAB1.png" },
+  TUTS: { name: "H. L. D. P. Champika", subjects: "Tutorial Sciences", img: "Image/TUTS.png" },
+  TUTH: { name: "M. V. H. D. Gunasekara", subjects: "Tutriols History", img: "Image/TUTH.png" },
+  TUSI: { name: "S. T. Indrani", subjects: "Tutorials Sinhala", img: "Image/TUSI.png" },
+  LIOB: { name: "MRS. Library", subjects: "Library", img: "Image/LIOB.png" }
 
 };
