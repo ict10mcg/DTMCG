8a83d4c107dca76175086fdbcb6fb0deecb92246
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-07-02 05:58:59 +0530

    Update bot.js

--- G11/java/bot.js
+++ G11/java/bot.js
@@ -3,21 +3,21 @@
     const periodTimings = [
     { start: "00:30", end: "07:49", id: "No School" },
     { start: "7:50", end: "8:30", id: "period1" }, 
-    { start: "8:31", end: "9:10", id: "period2" },
-    { start: "9:11", end: "9:50", id: "period3" },
-    { start: "9:51", end: "9:59", id: "period4" },
+    { start: "8:30", end: "9:10", id: "period2" },
+    { start: "9:10", end: "9:50", id: "period3" },
+    { start: "9:50", end: "10.00", id: "period4" },
     { start: "10:00", end: "10:30", id: "period4" },
 
 
-    { start: "10:31", end: "10:50", id: "period5" }, // Interval
+    { start: "10:30", end: "10:50", id: "period5" }, // Interval
 
 
     
-    { start: "10:51", end: "11:30", id: "period6" },
-    { start: "11:31", end: "12:10", id: "period7" },
-    { start: "12:11", end: "12:50", id: "period8" },
-    { start: "12:51", end: "13:30", id: "period9" },
-    { start: "13:31", end: "23:59", id: "No School" }
+    { start: "10:50", end: "11:30", id: "period6" },
+    { start: "11:30", end: "12:10", id: "period7" },
+    { start: "12:10", end: "12:50", id: "period8" },
+    { start: "12:50", end: "13:30", id: "period9" },
+    { start: "13:30", end: "23:59", id: "No School" }
 ];
 
 
