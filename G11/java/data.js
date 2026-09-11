b1df4e74eec8976250e366fe4ca8aac616b7e5b2
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2026-09-11 22:17:41 +0530

    Update data.js

--- G11/java/data.js
+++ G11/java/data.js
@@ -1,342 +1,42 @@
 const teacherDataByDay = {
     Monday: {
-        classA: {
-            period1:"SANP",
-            period2:"SANP",
-            period3:"MVHD",
-            period4:"GHDC",
-            period5:null,
-            period6:"HLDP",
-            period7:"SDJP",
-            period8:"OPT2A",
-            period9:"MACH", // New period
-        },
-        classB: {
-            period1:"STIN",
-            period2:"DGSR",
-            period3:"DGSR",
-            period4:"MKRC",
-            period5:null,
-            period6:"HRNG",
-            period7:"KHNL",
-            period8:"OPT2A",
-            period9:"KSRI", // New period
-        },
-        classC: {
-            period1:"WHG",
-            period2:"WHG",
-            period3:"HRNG",
-            period4:"BPLA",
-            period5:null,
-            period6:"MDMU",
-            period7:"MDMU",
-            period8:"OPT2A",
-            period9:"ASMA", // New period
-        },
-        classD: {
-            period1:"NUWA",
-            period2:"SDJP",
-            period3:"BDSE",
-            period4:"KSRI",
-            period5:null,
-            period6:"MVHD",
-            period7:"MVHD",
-            period8:"IAGA",
-            period9:"IAGA", // New period
-        },
-        classE: {
-            period1:"KSRI",
-            period2:"IUG",
-            period3:"GISA",
-            period4:"WARW",
-            period5:null,
-            period6:"SANP",
-            period7:"SANP",
-            period8:"NPAM",
-            period9:"NPAM", // New period
-        },
-        classF: {
-            period1:"SMYD",
-            period2:"YASK",
-            period3:"GHDC",
-            period4:"MACH",
-            period5:null,
-            period6:"UJTC",
-            period7:"HLDP",
-            period8:"HLDP",
-            period9:"YASK", // New period
-        },
+        classA: { period1: "92",   period2: "170", period3: "OP01", period4: "OP03", period5: null, period6: "106", period7: "75",  period8: "109" },
+        classB: { period1: "54",   period2: "54",  period3: "OP01", period4: "OP03", period5: null, period6: "136", period7: "61",  period8: "75" },
+        classC: { period1: "85",   period2: "90",  period3: "OP01", period4: "OP03", period5: null, period6: "29",  period7: "29",  period8: "201" },
+        classD: { period1: "163",  period2: "201", period3: "201",  period4: "90",   period5: null, period6: "130", period7: "69",  period8: "169" },
+        classE: { period1: "63",   period2: "131", period3: "131",  period4: "107",  period5: null, period6: "121", period7: "190", period8: "90" },
+        classF: { period1: "62",   period2: "131", period3: "131",  period4: "75",   period5: null, period6: "95",  period7: "95",  period8: "95" }
     },
     Tuesday: {
-        classA: {
-            period1:"SANP",
-            period2:"HLDP",
-            period3:"HLDP",
-            period4:"GHDC",
-            period5:null,
-            period6:"OPT3A",
-            period7:"MVHD",
-            period8:"OPT1A",
-            period9:"SDJP", // New period
-        },
-        classB: {
-            period1:"STIN",
-            period2:"STIN",
-            period3:"KHNL",
-            period4:"MKRC",
-            period5:null,
-            period6:"OPT3A",
-            period7:"DGSR",
-            period8:"OPT1A",
-            period9:"HRNG", // New period
-        },
-        classC: {
-            period1:"WHG",
-            period2:"HRNG",
-            period3:"MACH",
-            period4:"BPLA",
-            period5:null,
-            period6:"OPT3A",
-            period7:"ASMA",
-            period8:"OPT1A",
-            period9:"MDMU", // New period
-        },
-        classD: {
-            period1:"OPT1B",
-            period2:"OPT1B",
-            period3:"SDJP",
-            period4:"OPT2B",
-            period5:null,
-            period6:"NUWA",
-            period7:"NUWA",
-            period8:"KSRI",
-            period9:"BDSE", // New period
-        },
-        classE: {
-            period1:"OPT1B",
-            period2:"OPT1B",
-            period3:"IUG",
-            period4:"OPT2B",
-            period5:null,
-            period6:"WARW",
-            period7:"WARW",
-            period8:"NPAM",
-            period9:"GISA", // New period
-        },
-        classF: {
-            period1:"OPT1B",
-            period2:"OPT1B",
-            period3:"SMYD",
-            period4:"OPT2B",
-            period5:null,
-            period6:"HLDP",
-            period7:"HLDP",
-            period8:"YASK",
-            period9:"GHDC", // New period
-        },
+        classA: { period1: "92",   period2: "170", period3: "OP03", period4: "106",  period5: null, period6: "106", period7: "OP02",period8: "109" },
+        classB: { period1: "54",   period2: "109", period3: "OP03", period4: "75",   period5: null, period6: "OP02", period7: "OP02",period8: "75" },
+        classC: { period1: "85",   period2: "130", period3: "OP03", period4: "130",  period5: null, period6: "201", period7: "OP02",period8: "201" },
+        classD: { period1: "163",  period2: "201", period3: "OP01", period4: "OP01", period5: null, period6: "90",  period7: "69",  period8: "69" },
+        classE: { period1: "63",   period2: "63",  period3: "131",  period4: "OP01", period5: null, period6: "107", period7: "90",  period8: "190" },
+        classF: { period1: "62",   period2: "75",  period3: "OP01", period4: "75",   period5: null, period6: "78",  period7: "78",  period8: "95" }
     },
     Wednesday: {
-        classA: {
-            period1:"SANP",
-            period2:"GHDC",
-            period3:"GHDC",
-            period4:"HLDP",
-            period5:null,
-            period6:"SDJP",
-            period7:"MVHD",
-            period8:"OPT1A",
-            period9:"OPT1A", // New period
-        },
-        classB: {
-            period1:"STIN",
-            period2:"MKRC",
-            period3:"MKRC",
-            period4:"KHNL",
-            period5:null,
-            period6:"DGSR",
-            period7:"IUG",
-            period8:"OPT1A",
-            period9:"OPT1A", // New period
-        },
-        classC: {
-            period1:"WHG",
-            period2:"BPLA",
-            period3:"BPLA",
-            period4:"MDMU",
-            period5:null,
-            period6:"IUG",
-            period7:"ASMA",
-            period8:"OPT1A",
-            period9:"OPT1A", // New period
-        },
-        classD: {
-            period1:"SDJP",
-            period2:"OPT3B",
-            period3:"IAGA",
-            period4:"IAGA",
-            period5:null,
-            period6:"NUWA",
-            period7:"NUWA",
-            period8:"HKAU",
-            period9:"HKAU", // New period
-        },
-        classE: {
-            period1:"NPAM",
-            period2:"OPT3B",
-            period3:"KSRI",
-            period4:"SWM",
-            period5:null,
-            period6:"NPAM",
-            period7:"SWM",
-            period8:"GISA",
-            period9:"GISA", // New period
-        },
-        classF: {
-            period1:"SMYD",
-            period2:"OPT3B",
-            period3:"UJTC",
-            period4:"UJTC",
-            period5:null,
-            period6:"SWM",
-            period7:"HLDP",
-            period8:"GHDC",
-            period9:"GHDC", // New period
-        },
+        classA: { period1: "92",   period2: "106", period3: "106",  period4: "106",  period5: null, period6: "75",  period7: "170", period8: "170" },
+        classB: { period1: "54",   period2: "109", period3: "75",   period4: "75",   period5: null, period6: "61",  period7: "136", period8: "54" },
+        classC: { period1: "85",   period2: "29",  period3: "130",  period4: "130",  period5: null, period6: "201", period7: "90",  period8: "157" },
+        classD: { period1: "163",  period2: "69",  period3: "OP03", period4: "OP03", period5: null, period6: "OP02",period7: "201", period8: "OP01" },
+        classE: { period1: "63",   period2: "107", period3: "OP03", period4: "OP03", period5: null, period6: "OP02",period7: "121", period8: "OP01" },
+        classF: { period1: "62",   period2: "75",  period3: "OP03", period4: "OP03", period5: null, period6: "OP02",period7: "190", period8: "OP01" }
     },
     Thursday: {
-        classA: {
-            period1:"SANP",
-            period2:"HLDP",
-            period3:"HLDP",
-            period4:"MVHD",
-            period5:null,
-            period6:"OPT3A",
-            period7:"OPT3A",
-            period8:"GHDC",
-            period9:"SDJP", // New period
-        },
-        classB: {
-            period1:"DGSR",
-            period2:"DGSR",
-            period3:"STIN",
-            period4:"HRNG",
-            period5:null,
-            period6:"OPT3A",
-            period7:"OPT3A",
-            period8:"MKRC",
-            period9:"KHNL", // New period
-        },
-        classC: {
-            period1:"MDMU",
-            period2:"WHG",
-            period3:"HRNG",
-            period4:"MACH",
-            period5:null,
-            period6:"OPT3A",
-            period7:"OPT3A",
-            period8:"BPLA",
-            period9:"WHG", // New period
-        },
-        classD: {
-            period1:"SDJP",
-            period2:"NUWA",
-            period3:"BDSE",
-            period4:"BDSE",
-            period5:null,
-            period6:"MVHD",
-            period7:"MVHD",
-            period8:"IAGA",
-            period9:"IAGA", // New period
-        },
-        classE: {
-            period1:"IUG",
-            period2:"NAMP",
-            period3:"GISA",
-            period4:"GISA",
-            period5:null,
-            period6:"NPAM",
-            period7:"IUG",
-            period8:"WARW",
-            period9:"SANP", // New period
-        },
-        classF: {
-            period1:"HLDP",
-            period2:"MACH",
-            period3:"GHDC",
-            period4:"GHDC",
-            period5:null,
-            period6:"UJTC",
-            period7:"YASK",
-            period8:"YASK",
-            period9:"SMYD", // New period
-        },
+        classA: { period1: "92",   period2: "92",  period3: "75",   period4: "106",  period5: null, period6: "60",  period7: "55",   period8: "170" },
+        classB: { period1: "54",   period2: "109", period3: "136",  period4: "190",  period5: null, period6: "61",  period7: "61",  period8: "62" },
+        classC: { period1: "85",   period2: "201", period3: "90",   period4: "130",  period5: null, period6: "201", period7: "29",  period8: "157" },
+        classD: { period1: "163",  period2: "69",  period3: "190",  period4: "201",  period5: null, period6: "90",  period7: "130", period8: "130" },
+        classE: { period1: "63",   period2: "107", period3: "131",  period4: "131",  period5: null, period6: "121", period7: "190", period8: "107" },
+        classF: { period1: "62",   period2: "78",  period3: "131",  period4: "131",  period5: null, period6: "95",  period7: "78",  period8: "75" }
     },
     Friday: {
-        classA: {
-            period1:"SANP",
-            period2:"SDJP",
-            period3:"OPT2A",
-            period4:"OPT2A",
-            period5:null,
-            period6:"HLDP",
-            period7:"GHDC",
-            period8:"GHDC",
-            period9:"MACH", // New period
-        },
-        classB: {
-            period1:"STIN",
-            period2:"IUG",
-            period3:"OPT2A",
-            period4:"OPT2A",
-            period5:null,
-            period6:"DGSR",
-            period7:"MKRC",
-            period8:"MKRC",
-            period9:"KSRI", // New period
-        },
-        classC: {
-            period1:"WHG",
-            period2:"ASMA",
-            period3:"OPT2A",
-            period4:"OPT2A",
-            period5:null,
-            period6:"HRNG",
-            period7:"BPLA",
-            period8:"BPLA",
-            period9:"MDMU", // New period
-        },
-        classD: {
-            period1:"SDJP",
-            period2:"IAJA",
-            period3:"OPT3B",
-            period4:"OPT3B",
-            period5:null,
-            period6:"OPT2B",
-            period7:"OPT2B",
-            period8:"OPT1B",
-            period9:"BDSE", // New period
-        },
-        classE: {
-            period1:"IUG",
-            period2:"SANP",
-            period3:"OPT3B",
-            period4:"OPT3B",
-            period5:null,
-            period6:"OPT2B",
-            period7:"OPT2B",
-            period8:"OPT1B",
-            period9:"GISA", // New period
-        },
-        classF: {
-            period1:"SMYD",
-            period2:"HLDP",
-            period3:"OPT3B",
-            period4:"OPT3B",
-            period5:null,
-            period6:"OPT2B",
-            period7:"OPT2B",
-            period8:"OPT1B",
-            period9:"GHDC", // New period
-        },
-    },
+        classA: { period1: "54",   period2: "92",  period3: "106",  period4: "170",  period5: null, period6: "60",  period7: "75",   period8: "OP01" },
+        classB: { period1: "54",   period2: "136", period3: "136",  period4: "190",  period5: null, period6: "61",  period7: "109", period8: "62" },
+        classC: { period1: "85",   period2: "90",  period3: "130",  period4: "130",  period5: null, period6: "201", period7: "29",  period8: "OP01" },
+        classD: { period1: "163",  period2: "130", period3: "190",  period4: "69",   period5: null, period6: "OP02",period7: "201", period8: "90" },
+        classE: { period1: "63",   period2: "63",  period3: "131",  period4: "107",  period5: null, period6: "OP02",period7: "90",  period8: "121" },
+        classF: { period1: "190",  period2: "78",  period3: "131",  period4: "75",   period5: null, period6: "OP02",period7: "95",  period8: "95" }
+    }
 };
